19dd478629ec99102a54ca7ecd4acbffb7fa084d
samliweisen <liweisen007@gmail.com>
2017-08-25 18:09:12 +0000

    add production push code to heroku

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const app = express();
 
-const path = require('path');
+// Run the app by serving the static files
+// in the dist directory
 
-// ...
-
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
-
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default
 // Heroku port
