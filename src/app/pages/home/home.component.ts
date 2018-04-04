4c1a43ebdb86ce4b41174ab2457fb4191cbde89b
samliweisen <liweisen007@gmail.com>
2018-04-04 20:12:26 +0000

    add air now tvs on home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Movies } from '../../models/movies';
 import { MovieService } from '../../services/movie.service';
+import { TvService } from '../../services/tv.service';
 
 @Component({
   selector: 'app-home',
@@ -10,14 +11,24 @@ import { MovieService } from '../../services/movie.service';
 })
 export class HomeComponent implements OnInit {
   movies: Movies;
+  tvs: Movies;
 
-  constructor(private movieService: MovieService) { }
+  constructor(
+    private movieService: MovieService,
+    private tvService: TvService
+  ) { }
 
   ngOnInit() {
     this.movieService.getMovies('now_playing', 1).then(movies => {
       const list = movies.results.slice(0, 6);
       movies.results = list;
       this.movies = movies;
+    });
+    
+    this.tvService.getTvs('airing_today', 1).then(tvs => {
+      const list = tvs.results.slice(0, 6);
+      tvs.results = list;
+      this.tvs = tvs;
     });
   }
 
