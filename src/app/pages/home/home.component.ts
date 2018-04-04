6f045e998c3b82c208617b92fbde249fbd6f2ffe
samliweisen <liweisen007@gmail.com>
2018-04-04 19:48:32 +0000

    display popular movies at home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Movies } from '../../models/movies';
+import { MovieService } from '../../services/movie.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  movies: Movies;
 
-  constructor() { }
+  constructor(private movieService: MovieService) { }
 
   ngOnInit() {
+    this.movieService.getMovies('now_playing', 1).then(movies => {
+      const list = movies.results.slice(0, 6);
+      movies.results = list;
+      this.movies = movies;
+    });
   }
 
 }
