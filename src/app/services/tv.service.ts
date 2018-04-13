e1cbeb9042cfcf4cb81a36f9a8ca76f6cce4eae4
samliweisen <liweisen007@gmail.com>
2018-04-13 02:41:00 +0000

    add get tv detail service

--- src/app/services/tv.service.ts
+++ src/app/services/tv.service.ts
@@ -24,6 +24,12 @@ export class TvService {
         return tvs;
     }
     
+    getDetail(id: number) {
+        const url = this.baseUrl + id + this.apiKey;
+        const tv = this.http.get(url).toPromise().then(res => res.json()).catch(this.handleError);
+        return tv;
+    }
+    
     private handleError(error: any): Promise<any> {
         console.error('An error occurred', error); // for demo purposes only
         return Promise.reject(error.message || error);
