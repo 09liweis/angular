47a00a5a8eadf34d39eb6575202c8c465927d74f
samliweisen <liweisen007@gmail.com>
2018-04-14 02:55:56 +0000

    get function to handle cast image

--- src/app/components/person/person.component.ts
+++ src/app/components/person/person.component.ts
@@ -12,5 +12,12 @@ export class PersonComponent implements OnInit {
 
   ngOnInit() {
   }
+  getCastImage(path) {
+    if (path) {
+      return 'https://image.tmdb.org/t/p/w500' + path;
+    } else {
+      return 'https://netbranding.co.nz/wp-content/uploads/2014/04/avatar-2.png';
+    }
+  }
 
 }
