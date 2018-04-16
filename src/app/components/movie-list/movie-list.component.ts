769f87e11516d8274f3b3abb6bf3aa64b511bf09
samliweisen <liweisen007@gmail.com>
2018-04-16 22:59:19 +0000

    add animation to movies list

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -1,10 +1,27 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+
 import { Movies } from '../../models/movies';
 
 @Component({
   selector: 'movie-list',
   templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.css']
+  styleUrls: ['./movie-list.component.css'],
+  animations: [
+    trigger('moviesAnimation', [
+      transition('* => *', [
+
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+
+        query(':enter', stagger('300ms', [
+          animate('1s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+          ]))]), {optional: true})
+      ])
+    ])
+  ]
 })
 export class MovieListComponent implements OnInit {
   @Input() type: string;// current type of movie or tv
