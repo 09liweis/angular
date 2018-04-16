50e47d12ed6753b1c35b9c0cf273e2866c16a380
samliweisen <liweisen007@gmail.com>
2018-04-16 23:05:00 +0000

    update movie list animation value

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -13,11 +13,18 @@ import { Movies } from '../../models/movies';
 
         query(':enter', style({ opacity: 0 }), {optional: true}),
 
-        query(':enter', stagger('300ms', [
-          animate('1s ease-in', keyframes([
-            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+        query(':enter', stagger('200ms', [
+          animate('0.5s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-35%)', offset: 0}),
             style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
             style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+          ]))]), {optional: true}),
+        
+        query(':leave', stagger('300ms', [
+          animate('1s ease-in', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 0, transform: 'translateY(-35%)',     offset: 1.0}),
           ]))]), {optional: true})
       ])
     ])
