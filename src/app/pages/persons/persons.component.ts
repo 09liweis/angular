cc2c5638002e6d67bb8668df199ba6b0c6e6ec46
samliweisen <liweisen007@gmail.com>
2018-04-18 01:53:50 +0000

    add hover effect on person

--- src/app/pages/persons/persons.component.ts
+++ src/app/pages/persons/persons.component.ts
@@ -16,7 +16,7 @@ import { PersonService } from '../../services/person.service';
     trigger('personsAnimation', [
       transition('* => *', [
         query(':enter', style({opacity: 0}), {optional: true}),
-        query(':enter', stagger('300ms', [
+        query(':enter', stagger('200ms', [
           animate('0.3s ease', keyframes([
             style({opacity: 0, transform: 'translateX(20%)', offset: 0}),
             style({opacity: 0.5, transform: 'translateX(-20px)', offset: 0.5}),
