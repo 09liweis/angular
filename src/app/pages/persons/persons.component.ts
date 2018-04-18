515a7d24eedaf35c24f33e24a22ee078ded49421
samliweisen <liweisen007@gmail.com>
2018-04-18 01:49:30 +0000

    add animation to persons page

--- src/app/pages/persons/persons.component.ts
+++ src/app/pages/persons/persons.component.ts
@@ -2,6 +2,8 @@ import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, Params }   from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+
 import { Person } from '../../models/person/person';
 import { Persons } from '../../models/person/persons';
 import { PersonService } from '../../services/person.service';
@@ -9,7 +11,21 @@ import { PersonService } from '../../services/person.service';
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html',
-  styleUrls: ['./persons.component.css']
+  styleUrls: ['./persons.component.css'],
+  animations: [
+    trigger('personsAnimation', [
+      transition('* => *', [
+        query(':enter', style({opacity: 0}), {optional: true}),
+        query(':enter', stagger('300ms', [
+          animate('0.3s ease', keyframes([
+            style({opacity: 0, transform: 'translateX(20%)', offset: 0}),
+            style({opacity: 0.5, transform: 'translateX(-20px)', offset: 0.5}),
+            style({opacity: 1, transform: 'translateX(0)', offset: 1}),
+          ]))
+        ]), {optional: true})
+      ])
+    ])
+  ]
 })
 export class PersonsComponent implements OnInit {
 
