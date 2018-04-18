8627b145bc9a96875c7578ebd78c19add70975dc
samliweisen <liweisen007@gmail.com>
2018-04-18 20:15:15 +0000

    add search api endpoint

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -7,7 +7,10 @@ import { Movies } from '../models/movies';
 
 @Injectable()
 export class SearchService {
-
-  constructor() { }
+    private baseUrl = 'https://api.themoviedb.org/3/search/multi';
+    private apiKey = '?api_key=8109b23cc9abaf02cf3c699ec62ccc19';
+    private headers = new Headers({'Content-Type': 'application/json'});
+    
+    constructor() { }
 
 }
