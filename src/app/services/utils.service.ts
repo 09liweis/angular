4e7ae826c7b964f91bb4b90f5f70b6bbb4dd65af
samliweisen <liweisen007@gmail.com>
2018-04-25 15:05:48 +0000

    finish formateTitle function

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -6,7 +6,10 @@ export class UtilsService {
   constructor() { }
   
   formateTitle(title: string): string {
-      
+      //formate 'now_playing' to 'Noew Playing'
+      return title.split('_').map((word) => {
+          return word.charAt(0).toUpperCase() + word.slice(1);
+      }).join(' ');
   }
 
 }
