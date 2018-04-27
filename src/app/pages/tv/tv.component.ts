eb6d5800a6d9e3c2b56f84716a30eba04260284f
samliweisen <liweisen007@gmail.com>
2018-04-27 16:56:07 +0000

    import title module

--- src/app/pages/tv/tv.component.ts
+++ src/app/pages/tv/tv.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params }   from '@angular/router';
+import { Title, DomSanitizer } from '@angular/platform-browser';
 
 import { Movie } from '../../models/movie';
 import { TvService } from '../../services/tv.service';
@@ -14,12 +15,14 @@ export class TvComponent implements OnInit {
   constructor(
     private tvService: TvService,
     private route: ActivatedRoute,
+    private titleService: Title
   ) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.tvService.getDetail(+params['id']).subscribe(tv => {
         this.tv = tv;
+        console.log(tv);
       })
     })
   }
