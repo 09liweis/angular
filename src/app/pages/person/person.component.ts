1cdb8c1375047c2b826a576a59d48ef567ac4666
samliweisen <liweisen007@gmail.com>
2018-04-30 16:58:24 +0000

    import route and title module

--- src/app/pages/person/person.component.ts
+++ src/app/pages/person/person.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Title, DomSanitizer } from '@angular/platform-browser';
 
 import { PersonService } from '../../services/person.service';
 
@@ -10,7 +12,9 @@ import { PersonService } from '../../services/person.service';
 export class PersonPageComponent implements OnInit {
 
   constructor(
-    private personService: PersonService
+    private personService: PersonService,
+    private route: ActivatedRoute,
+    private titleService: Title
   ) { }
 
   ngOnInit() {
