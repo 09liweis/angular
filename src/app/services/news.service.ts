c4c0e41e63f3ed058221272b8bb3f8ef14850bae
samliweisen <liweisen007@gmail.com>
2018-05-02 20:26:25 +0000

    remove handleerror from news service

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -21,10 +21,4 @@ export class NewsService {
         const news = this.http.get(url);
         return news;
     }
-    
-    private handleError(error: any): Promise<any> {
-        console.error('An error occurred', error); // for demo purposes only
-        return Promise.reject(error.message || error);
-    }
-
 }
