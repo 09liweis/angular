4743caf0d245ae918dd28753872b0b6627c6d28b
samliweisen <liweisen007@gmail.com>
2018-05-03 14:13:33 +0000

    add utilsService to tabs component

--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { UtilsService } from '../../services/utils.service';
+
 @Component({
   selector: 'tabs',
   templateUrl: './tabs.component.html',
@@ -9,9 +11,13 @@ export class TabsComponent implements OnInit {
   @Input() links: Array<string>;
   @Input() currentLink: string; 
 
-  constructor() { }
+  constructor(private utilsService: UtilsService) { }
 
   ngOnInit() {
+  }
+  
+  formatTitle(title: string): string {
+    return this.utilsService.formatTitle(title);
   }
 
 }
