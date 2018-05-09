d8c39a75f678323773a988599262a29429f45291
samliweisen <liweisen007@gmail.com>
2018-05-09 17:09:13 +0000

    update homepage compoennt to use sass

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,7 +7,7 @@ import { TvService } from '../../services/tv.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   movies: Movies;
