2ab945a3f8c590f14e23ea9d7babc1948420ed04
samliweisen <liweisen007@gmail.com>
2018-05-17 18:30:46 +0000

    video.actions

---
+++ src/app/actions/video.actoins.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Action } from '@ngrx/store';
+import { Video } from './models/video';
+
+export const ADD_VIDEO = 'ADD_VIDEO';
+
+export class AddVideo implements Action {
+    readonly type = ADD_VIDEO;
+    constructor(public payload: Video) {}
+}
+
+export type Actions = AddVideo;
