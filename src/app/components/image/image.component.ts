ca36789a8eedbf66bd9e2e49c9b6fd802e44c8db
samliweisen <liweisen007@gmail.com>
2018-05-22 19:07:04 +0000

    add alt input

--- src/app/components/image/image.component.ts
+++ src/app/components/image/image.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-image',
+  selector: 'image',
   templateUrl: './image.component.html',
   styleUrls: ['./image.component.scss']
 })
 export class ImageComponent implements OnInit {
   @Input() path: String;
+  @Input() alt: String;
   constructor() { }
 
   ngOnInit() {
