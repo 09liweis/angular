d2ce2e4988e3aeca252babc2bb5c74d2785562b0
samliweisen <liweisen007@gmail.com>
2018-05-22 19:08:22 +0000

    add caption input

--- src/app/components/image/image.component.ts
+++ src/app/components/image/image.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ImageComponent implements OnInit {
   @Input() path: String;
   @Input() alt: String;
+  @Input() caption: String;
   constructor() { }
 
   ngOnInit() {
