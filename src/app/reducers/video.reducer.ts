cfb4731ab1d026cdb981a37507b3ad9250b4c6a6
samliweisen <liweisen007@gmail.com>
2018-06-04 17:53:24 +0000

    add intital state

--- src/app/reducers/video.reducer.ts
+++ src/app/reducers/video.reducer.ts
@@ -2,6 +2,15 @@ import { Action } from '@ngrx/store'
 import { Video } from '../models/video'
 import * as VideoActions from '../actions/video.actions'
 
+export const INCREMENT = 'INCREMENT';
+
+const initialState = {
+    videos: [],
+    language: 'en',
+    country: 'US'
+};
+
+
 export function reducer(state: Video[] = [], action: VideoActions.Actions) {
     switch(action.type) {
         case VideoActions.ADD_VIDEO:
