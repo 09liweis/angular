1a49929a49624ed2d32eaa8ff69ce544a6888771
samliweisen <liweisen007@gmail.com>
2018-06-12 19:15:38 +0000

    fill cast model for person page

---
+++ src/app/models/person/cast.ts
@@ -0,0 +1,19 @@
+export class Cast {
+    id: number;
+    title: string;
+    name: string;//for tv
+    poster_path: string;
+    adult: boolean;
+    overview: string;
+    release_date: string;
+    genre_ids: Array<number>;
+    original_title: string;
+    backdrop_path: string;
+    popularity: number;
+    vote_count: number;
+    video: boolean;
+    vote_average: number;
+    homepage: string;
+    imdb_id: string;
+    credit_id: string;
+}
