1ca6985c1758e2103e8d01cfb7d4496c7c1e6faf
samliweisen <liweisen007@gmail.com>
2018-06-14 14:30:02 +0000

    populate gallery component

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+
+import { MovieImage } from '../../models/movieImage';
 
 @Component({
   selector: 'app-gallery',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
+  images: MovieImage;
+  modalOpen: boolean = false;
+  currentImage: any = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  previewImage(image, index) {
+    this.currentImage = index;
+    this.modalOpen = true;
+  }
+  closeModal() {
+    this.modalOpen = false;
+  }
+  @HostListener('document:keyup', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) { 
+    if (event.key == 'ArrowRight') {
+      if (this.currentImage == this.images.posters.length - 1) {
+        this.currentImage = 0
+      } else {
+        this.currentImage += 1;
+      }
+    }
+    if (event.key == 'ArrowLeft') {
+      if (this.currentImage == 0) {
+        this.currentImage = this.images.posters.length - 1;
+      } else {
+        this.currentImage -= 1;
+      }
+    }
   }
 
 }
