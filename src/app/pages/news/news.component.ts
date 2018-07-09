53cb3ed00174aeaaa0ac161b1c2df89a222f3cc7
samliweisen <liweisen007@gmail.com>
2018-07-09 19:44:48 +0000

    add scroll to top on news page

--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -34,6 +34,7 @@ export class NewsComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    window.scrollTo(0, 0);
     this.newsService.getNewsFromRottentomatoes()
       .subscribe(news => {
         this.news = news;
