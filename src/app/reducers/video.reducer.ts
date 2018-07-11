acadeb1fa7abc29ad93b093badcab7c019e98a18
samliweisen <liweisen007@gmail.com>
2018-07-11 19:23:57 +0000

    update reducer

--- src/app/reducers/video.reducer.ts
+++ src/app/reducers/video.reducer.ts
@@ -2,8 +2,6 @@ import { Action } from '@ngrx/store'
 import { Video } from '../models/video'
 import * as VideoActions from '../actions/video.actions'
 
-export const INCREMENT = 'INCREMENT';
-
 // const initialState = {
 //     videos: [],
 //     count: 0,
@@ -19,12 +17,10 @@ const initialState: Video = {
 }
 
 
-export function reducer(state: number = 0, action: Action) {
+export function reducer(state: Video[] = [initialState], action: VideoActions.Actions) {
     switch(action.type) {
-        case INCREMENT: {
-            console.log(state + 1);
-            return state + 1;
-        }
+        case VideoActions.ADD_VIDEO:
+            return [...state, action.payload];
         default:
             return state;
     }
