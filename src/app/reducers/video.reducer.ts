cf257a94edefe9a6b1ffe47b0f0a64d778a279a0
samliweisen <liweisen007@gmail.com>
2018-07-11 19:01:56 +0000

    intital example video

--- src/app/reducers/video.reducer.ts
+++ src/app/reducers/video.reducer.ts
@@ -4,12 +4,19 @@ import * as VideoActions from '../actions/video.actions'
 
 export const INCREMENT = 'INCREMENT';
 
-const initialState = {
-    videos: [],
-    count: 0,
-    language: 'en',
-    country: 'US'
-};
+// const initialState = {
+//     videos: [],
+//     count: 0,
+//     language: 'en',
+//     country: 'US'
+// };
+
+const initialState: Video = {
+    id: 123455,
+    title: 'Movie Title',
+    type: 'movie',
+    src: 'dfkajsdfladkjf'
+}
 
 
 export function reducer(state: number = 0, action: Action) {
