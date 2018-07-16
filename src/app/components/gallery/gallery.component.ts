151dbc8d2316257a9a0126d79b9c835316e873ba
samliweisen <liweisen007@gmail.com>
2018-07-16 15:45:28 +0000

    add animatio for gallery images

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -4,7 +4,22 @@ import { trigger,style,transition,animate,keyframes,query,stagger } from '@angul
 @Component({
   selector: 'gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+  styleUrls: ['./gallery.component.scss'],
+  animations: [
+    trigger('galleryAnimation', [
+      transition('* => *', [
+
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+
+        query(':enter', stagger('200ms', [
+          animate('0.3s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-35%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+          ]))]), {optional: true})
+      ])
+    ])
+  ]
 })
 export class GalleryComponent implements OnInit {
   @Input() images;
