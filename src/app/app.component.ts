61ffd33dbbd35abd84211912a2896a58f217fcb8
09liweis <weisen.li@hotmail.com>
2024-05-15 11:36:05 -0400

    add mapbox, tailwindcss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: "app-root",
@@ -10,4 +11,16 @@ import { RouterOutlet } from "@angular/router";
 })
 export class AppComponent {
   title = "world";
+  map: mapboxgl.Map | undefined;
+  lat: number = 30.2672;
+  lng: number = -97.7431;
+  ngOnInit() {
+    this.map = new mapboxgl.Map({
+      accessToken: 'pk.eyJ1Ijoic2FtbGl3ZWlzZW4iLCJhIjoiY2twZWR6ZzZyMDZtbTJybzJsbWxoYW1sOCJ9.lhs_TcMlrQEiKk6LAGxo2A',
+      container: 'main',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      zoom: 13,
+      center: [this.lng, this.lat]
+    });
+  }
 }
